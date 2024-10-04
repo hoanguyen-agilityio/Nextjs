0dd89ab78f56e3aa83fc7e978dbfa4968bb3ce6c
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-04 15:47:45 +0700

    feat: Implement icon analytics component

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,3 +3,4 @@ export { default as DashboardIcon } from './Dashboard';
 export { default as InboxIcon } from './Inbox';
 export { default as WalletIcon } from './Wallet';
 export { default as NewsFeedIcon } from './NewsFeed';
+export { default as AnalyticsIcon } from './Analytics';
