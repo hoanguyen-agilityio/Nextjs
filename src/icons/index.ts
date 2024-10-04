0de02ade93275272bbf3ecaa40e439e562051cae
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-04 15:46:18 +0700

    feat: Implement icon news feed component

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from './Logo';
 export { default as DashboardIcon } from './Dashboard';
 export { default as InboxIcon } from './Inbox';
 export { default as WalletIcon } from './Wallet';
+export { default as NewsFeedIcon } from './NewsFeed';
