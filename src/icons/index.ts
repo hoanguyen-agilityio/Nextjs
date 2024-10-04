2e5f8e3454f7b96a070034a2eb7e8cf5f4b5730c
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-04 15:43:54 +0700

    feat: Implement icon wallet component

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as DashboardIcon } from './Dashboard';
 export { default as InboxIcon } from './Inbox';
+export { default as WalletIcon } from './Wallet';
