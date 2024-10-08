db4f165691c9a02645a4d1eac064445894446bb1
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-08 18:50:56 +0700

    fix: Use variables for color coding

--- src/Constant/Menu.tsx
+++ src/Constant/Menu.tsx
@@ -14,7 +14,7 @@ export const MENU = [
   {
     icon: <Dashboard width="30px" height="30px" />,
     label: 'Dashboard',
-    className: 'text-[#12AFF0] bg-[#12AFF033]',
+    className: 'text-blue-400 bg-translucent-400',
     href: 'dashboard',
   },
   {
