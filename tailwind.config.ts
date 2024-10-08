7d8fa61d128b6e01d0ae3b9f9206b90c30844fec
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-08 09:06:17 +0700

    fix: Use colors and fontSize, instead of colors : {...colors} and size: {...fontSize}

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,12 +14,8 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      size: {
-        ...fontSize
-      },
-      colors: {
-        ...colors,
-      },
+      fontSize,
+      colors
     },
   },
   plugins: [nextui()],
