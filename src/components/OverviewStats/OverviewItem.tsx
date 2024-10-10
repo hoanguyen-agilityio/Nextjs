8776d0d6174fd1a7bc54bafd7576a7292ae5d2b9
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-10 11:04:06 +0700

    feat: Use memo for OverviewItem component

--- src/components/OverviewStats/OverviewItem.tsx
+++ src/components/OverviewStats/OverviewItem.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { ChartArrows } from '@/icons';
 
 interface OverviewItemProps {
@@ -7,19 +8,23 @@ interface OverviewItemProps {
   isLast: boolean;
 }
 
-const OverviewItem = ({ value, label, growth, isLast }: OverviewItemProps) => (
-  <div
-    className={`flex flex-col items-center ${!isLast ? 'border-r border-gray-700 pr-6xl' : ''}`}
-  >
-    <span className="text-4xl font-semibold mb-2sm">{value}</span>
-    <span className="text-base font-semibold text-grayBlue-400 mb-2sm">
-      {label}
-    </span>
-    <div className="flex items-center rounded-lg bg-gray-50 py-sm px-m">
-      <ChartArrows width="12px" height="7px" fill="black" className="mr-1" />
-      <span className="text-sm font-bold">{growth}</span>
+const OverviewItem = memo(
+  ({ value, label, growth, isLast }: OverviewItemProps) => (
+    <div
+      className={`flex flex-col items-center ${!isLast ? 'border-r border-gray-700 pr-6xl' : ''}`}
+    >
+      <span className="text-4xl font-semibold mb-2sm">{value}</span>
+      <span className="text-base font-semibold text-grayBlue-400 mb-2sm">
+        {label}
+      </span>
+      <div className="flex items-center rounded-lg bg-gray-50 py-sm px-m">
+        <ChartArrows width="12px" height="7px" fill="black" className="mr-1" />
+        <span className="text-sm font-bold">{growth}</span>
+      </div>
     </div>
-  </div>
+  ),
 );
+
+OverviewItem.displayName = 'OverviewItem';
 
 export default OverviewItem;
