739f8c50a7ef44034c9f082f5ae2ca8dc50cabe5
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-18 14:54:44 +0700

    fix: Update unit test for Pagination component

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -2,15 +2,33 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Pagination from '.';
 
 describe('Pagination Component', () => {
+  const mockOnPageChange = jest.fn();
+  const currentPage = 1;
+  const totalItems = 100;
+  const itemsPerPage = 10;
   test('render pagination', () => {
-    const { container } = render(<Pagination />);
+    const { container } = render(
+      <Pagination
+        currentPage={currentPage}
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+        onPageChange={mockOnPageChange}
+      />,
+    );
     expect(container).toMatchSnapshot();
   });
 
   test('handles button clicks', () => {
     const handleClick = jest.fn();
 
-    render(<Pagination />);
+    render(
+      <Pagination
+        currentPage={currentPage}
+        totalItems={totalItems}
+        itemsPerPage={itemsPerPage}
+        onPageChange={mockOnPageChange}
+      />,
+    );
 
     const previousButton = screen.getByText('Previous');
     const nextButton = screen.getByText('Next');
