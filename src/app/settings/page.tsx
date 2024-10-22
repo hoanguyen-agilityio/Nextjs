008641645f59fd47b7ecab1f7c5c51002d293641
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-22 14:27:48 +0700

    fix: Create a few more pages to navigate through

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,8 @@
+const SettingsPage = () => (
+  <section className="flex h-screen flex-col items-center justify-center gap-2">
+    <h2 className="text-4xl font-semibold">Settings Page</h2>
+    <p className="text-2xl">Coming soon!</p>
+  </section>
+);
+
+export default SettingsPage;
