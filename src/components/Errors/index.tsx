bf890073decf7c7db010146b4d1e14a3eea7ab7f
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-25 17:13:05 +0700

    feat: Using implicit return type in errors component

--- src/components/Errors/index.tsx
+++ src/components/Errors/index.tsx
@@ -8,32 +8,30 @@ type ErrorProps = {
   className?: string;
 };
 
-const Error = ({ onReset, message = MESSAGE.DEFAULT }: ErrorProps) => {
-  return (
-    <div className="container max-w-lg bg-white p-8 flex flex-col items-center justify-center h-full m-auto">
-      <h1 className="text-3xl font-bold text-red-500 mb-4">
-        Oops! Something went wrong.
-      </h1>
-      <p className="text-gray-600 text-xl">{message}</p>
+const Error = ({ onReset, message = MESSAGE.DEFAULT }: ErrorProps) => (
+  <div className="container max-w-lg bg-white p-8 flex flex-col items-center justify-center h-full m-auto">
+    <h1 className="text-3xl font-bold text-red-500 mb-4">
+      Oops! Something went wrong.
+    </h1>
+    <p className="text-gray-600 text-xl">{message}</p>
 
-      {onReset ? (
-        <button
-          onClick={onReset}
-          className="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-xl"
-        >
-          Reset
-        </button>
-      ) : (
-        <Link
-          href={ROUTERS.HOME}
-          className="pt-5 text-blue-400 text-xl hover:underline"
-          aria-label="Go to Home Page"
-        >
-          Go to Home Page
-        </Link>
-      )}
-    </div>
-  );
-};
+    {onReset ? (
+      <button
+        onClick={onReset}
+        className="mt-4 inline-block px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 text-xl"
+      >
+        Reset
+      </button>
+    ) : (
+      <Link
+        href={ROUTERS.HOME}
+        className="pt-5 text-blue-400 text-xl hover:underline"
+        aria-label="Go to Home Page"
+      >
+        Go to Home Page
+      </Link>
+    )}
+  </div>
+);
 
 export default Error;
