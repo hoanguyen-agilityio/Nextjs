c9998a5929dae684f6e9618f293b2b1fded5da5c
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-25 17:11:17 +0700

    feat: Using implicit return type in menu component

--- src/components/Common/Menu/index.tsx
+++ src/components/Common/Menu/index.tsx
@@ -2,27 +2,25 @@ import Link from 'next/link';
 import { twMerge } from 'tailwind-merge';
 import { MENU } from '@/constant';
 
-const Menu = () => {
-  return (
-    <nav className="px-6 border-b-2 border-gray-50 dark:border-gray-700">
-      <ul>
-        {MENU.map(({ icon, label, className, href }) => (
-          <li key={label}>
-            <Link
-              href={href}
-              className={twMerge(
-                'flex py-lg pl-2xl rounded-lg items-center cursor-pointer font-semibold text-grayBlue-400',
-                className,
-              )}
-            >
-              {icon}
-              <span className="ml-5">{label}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+const Menu = () => (
+  <nav className="px-6 border-b-2 border-gray-50 dark:border-gray-700">
+    <ul>
+      {MENU.map(({ icon, label, className, href }) => (
+        <li key={label}>
+          <Link
+            href={href}
+            className={twMerge(
+              'flex py-lg pl-2xl rounded-lg items-center cursor-pointer font-semibold text-grayBlue-400',
+              className,
+            )}
+          >
+            {icon}
+            <span className="ml-5">{label}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Menu;
