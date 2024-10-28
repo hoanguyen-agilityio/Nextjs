f2f5c6554a23a19cdaf9f2523aacbfc339241c9a
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-28 10:08:46 +0700

    feat: Write unit tests for files in the utils folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,12 +6,6 @@ export { default as ProductSearch } from './ProductSearch';
 export { default as ProductTableWrapper } from './ProductTableWrapper';
 export { default as Filter } from './Filter';
 export { default as Error } from './Errors';
-export { default as ButtonCustom } from './Common/Button';
-export { default as InputCustom } from './Common/Input';
-export { default as Menu } from './Common/Menu';
-export { default as Sidebar } from './Common/Sidebar';
-export { default as TableCustom } from './Common/TableCustom';
 
-export * from './Common/Table';
-export * from './Common/Select';
+export * from './Common';
 export * from './OverviewStats';
