d0fb190c6dfaf67531172093d5d7695ce5d945e8
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-29 16:54:23 +0700

    feat: Add storybook to OverviewSkeleton

---
+++ src/ui/Dashboard/Skeleton/OverviewSkeleton/OverviewSkeleton.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import OverviewSkeleton from '.';
+
+export default {
+  title: 'Dashboard/OverviewSkeleton',
+  component: OverviewSkeleton,
+} as Meta;
+
+type Story = StoryObj<typeof OverviewSkeleton>;
+
+export const OverviewSkeletonDefault: Story = {
+  args: {
+    productCount: 4,
+  },
+};
