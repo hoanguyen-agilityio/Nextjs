87c9ef3bde862daf64140b0a897007eaea02e1d6
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-30 10:42:53 +0700

    fix: Update unit test for OverviewStats component

--- src/ui/Dashboard/OverviewStats/OverviewStats/OverviewStats.test.tsx
+++ src/ui/Dashboard/OverviewStats/OverviewStats/OverviewStats.test.tsx
@@ -1,9 +1,10 @@
 import { render } from '@testing-library/react';
 import OverviewStats from '.';
+import { data } from '@/mocks';
 
 describe('OverviewStats Component', () => {
   test('render overview stats', () => {
-    const { container } = render(<OverviewStats />);
+    const { container } = render(<OverviewStats data={data} />);
     expect(container).toMatchSnapshot();
   });
 });
