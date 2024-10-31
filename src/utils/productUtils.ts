36c8640a63ac187ee52711596490cb0348a4e901
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-10-31 16:45:14 +0700

    fix: formalize number

--- src/utils/productUtils.ts
+++ src/utils/productUtils.ts
@@ -6,11 +6,13 @@ export const filterProducts = (
 ): IProducts[] => {
   if (!Array.isArray(products)) return [];
 
+  const parseNumber = (value: string) => Number(value.replace(/,/g, ''));
+
   switch (filter) {
     case 'views_below_5k':
-      return products.filter((item) => Number(item.views) < 5000);
+      return products.filter((item) => parseNumber(item.views) < 5000);
     case 'views_5k_plus':
-      return products.filter((item) => Number(item.views) >= 5000);
+      return products.filter((item) => parseNumber(item.views) >= 5000);
     default:
       return products;
   }
