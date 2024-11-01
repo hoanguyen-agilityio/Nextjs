bf1e9f7696ce0b031f09d7b394e13122f17aedca
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-11-01 10:19:17 +0700

    fix: Update error message variable when data fetch fails

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,7 +1,8 @@
 const EMPTY_TEXT = '';
 const MESSAGE = {
   ERROR_URL: 'PRODUCT_URL is undefined',
-  ERROR_GET_DATA: 'Error in getData: ',
+  ERROR_GET_DATA_PRODUCT: 'Error fetching product data: ',
+  ERROR_GET_DATA_OVERVIEW: 'Error fetching overview data:',
   DEFAULT:
     'We apologize for the inconvenience. The page you are looking for might have been removed or is temporarily unavailable.',
 };
