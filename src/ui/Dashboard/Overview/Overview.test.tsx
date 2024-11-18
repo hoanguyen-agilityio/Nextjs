49c4ff210252878b4f0e78bc3eefa2fc6e670c1c
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-11-18 13:52:25 +0700

    feat: Update unit test for Overview

--- src/ui/Dashboard/Overview/Overview.test.tsx
+++ src/ui/Dashboard/Overview/Overview.test.tsx
@@ -2,8 +2,8 @@ import { render } from '@testing-library/react';
 import Overview from '.';
 
 describe('Overview Component', () => {
-  test('render overview', () => {
-    const { container } = render(<Overview />);
+  test('renders Overview with mock data', async () => {
+    const { container } = render(await Overview());
     expect(container).toMatchSnapshot();
   });
 });
