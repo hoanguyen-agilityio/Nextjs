1fe5d10022a7902ad03228d0758d3497bb2e9c52
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-11-21 17:32:30 +0700

    fix: Update unit test for form component

--- src/components/common/Form/Form.test.tsx
+++ src/components/common/Form/Form.test.tsx
@@ -2,6 +2,12 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Form from '.';
 
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+  }),
+}));
+
 describe('Button component', () => {
   test('renders with default type', () => {
     const { container } = render(<Form modePage="add" label="Test Button" />);
