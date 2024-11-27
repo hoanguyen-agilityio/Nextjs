9896b35ff4f377d9905b08124bd2038365f0935e
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-11-27 17:03:36 +0700

    feat: Add responsive for login page

--- src/ui/Login/LoginFormSection/index.tsx
+++ src/ui/Login/LoginFormSection/index.tsx
@@ -7,13 +7,13 @@ import FormLogin from '../FormLogin';
 import { LogoWithoutText } from '@/icons';
 
 const LoginFormSection = () => (
-  <section className="flex items-center justify-around content-center min-h-screen">
-    <div className="flex flex-col items-center">
+  <section className="flex items-center justify-around content-center min-h-screen p-4">
+    <div className="flex flex-col items-center max-[1200px]:hidden">
       <LogoWithoutText width="163px" height="103px" />
-      <h1 className="text-white dark:text-black-900 font-semibold text-[62px]">
+      <h1 className="text-white dark:text-black-900 font-semibold text-[62px] text-center">
         Welcome to WunderUI
       </h1>
-      <span className="text-white dark:text-black-900 font-normal text-[26px]">
+      <span className="text-white dark:text-black-900 font-normal text-[26px] text-center">
         Sign In now to discover the full experience
       </span>
     </div>
