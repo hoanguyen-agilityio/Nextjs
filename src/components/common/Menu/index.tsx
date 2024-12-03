fc8d6455a0da26fdc4ebaee9cf53b83f711da79c
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2024-12-03 16:26:05 +0700

    fix: Update code to save state after clicking on menu

--- src/components/common/Menu/index.tsx
+++ src/components/common/Menu/index.tsx
@@ -1,29 +1,55 @@
+'use client';
+
 // Third party
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { twMerge } from 'tailwind-merge';
 
 // Constants
 import { MENU } from '@/constants';
 
-const Menu = () => (
-  <nav className="px-6 border-b-2 border-gray-50 dark:border-gray-700">
-    <ul>
-      {MENU.map(({ icon, label, className, href }) => (
-        <li key={label}>
-          <Link
-            href={href}
-            className={twMerge(
-              'flex py-lg pl-2xl rounded-lg items-center cursor-pointer text-ratio-200 dark:text-grayBlue-400',
-              className,
-            )}
-          >
-            {icon}
-            <span className="ml-5">{label}</span>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Menu = () => {
+  const [activeItem, setActiveItem] = useState<string>('Dashboard');
+
+  useEffect(() => {
+    const storedActiveItem = localStorage.getItem('activeMenuItem');
+    if (storedActiveItem) {
+      setActiveItem(storedActiveItem);
+    }
+  }, []);
+
+  const handleClick = (label: string) => {
+    setActiveItem(label);
+    localStorage.setItem('activeMenuItem', label);
+  };
+
+  return (
+    <nav className="px-6 border-b-2 border-gray-50 dark:border-gray-700">
+      <ul>
+        {MENU.map(({ icon, label, className, href }) => {
+          const isActive = activeItem === label;
+
+          return (
+            <li key={label}>
+              <Link
+                href={href}
+                onClick={() => handleClick(label)}
+                className={twMerge(
+                  'flex py-lg pl-2xl rounded-lg items-center cursor-pointer text-ratio-200 dark:text-grayBlue-400',
+                  isActive &&
+                    'text-ratio-100 bg-translucent-400 dark:text-blue-400',
+                  className,
+                )}
+              >
+                {icon}
+                <span className="ml-5">{label}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
